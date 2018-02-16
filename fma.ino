c8ec542fd43298f926f26527eb7ac52bc0bfc519
Oskar <oskar4711@gmail.com>
2018-02-16 09:42:39 +0000

    Update fma.ino

--- fma.ino
+++ fma.ino
@@ -1,1 +1,14 @@
+/******************************
+* test ino for me allone!     *
+******************************/
 
+setup() {
+  // set me up
+}
+loop() {
+  // rund the void vorever
+  runme;
+}
+void runme() {
+  // nothing to say
+}
